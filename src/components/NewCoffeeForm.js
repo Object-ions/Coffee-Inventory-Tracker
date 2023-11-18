a34d9a13de9e7a2e60528e6da9e2f48a44143cd2
object-ions <moshikoatia@gmail.com>
2023-11-17 16:40:04 -0800

    Adjust project file structure. Add new empty components

---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function newCoffeeForm(props) {
+  return (
+    <React.Fragment>
+      <h3>This is a form</h3>
+    </React.Fragment>
+  );
+}
+
+export default newCoffeeForm;
