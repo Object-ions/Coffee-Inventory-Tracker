da2800946ab40e5351c41e54e0915890c4731d91
object-ions <moshikoatia@gmail.com>
2023-11-18 16:49:56 -0800

    Add componenet- Detail

---
+++ src/components/CoffeeDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CoffeeDetail(props) {
+  return (
+    <React.Fragment>
+      <h1>Coffee Detail</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default CoffeeDetail;
