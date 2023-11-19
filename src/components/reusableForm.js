14b9bf67bff2fbf148811483797c3b9f8ba22c48
object-ions <moshikoatia@gmail.com>
2023-11-19 14:21:27 -0800

    WIP - working on bug - when clicking 'update' or 'return' dont triggeted action

--- src/components/reusableForm.js
+++ src/components/reusableForm.js
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ReusableForm(props) {
+
+  const { defaultValues } = props;
+
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <input type='text' name='name' placeholder='Coffee name' required/>
-        <input type='text' name='origin' placeholder='Coffee origin' required/>
+        <input type='text' name='name' placeholder='Coffee name' defaultValue={defaultValues ? defaultValues.name : ''} required/>
+        <input type='text' name='origin' placeholder='Coffee origin' defaultValue={defaultValues ? defaultValues.origin : ''} required/>
         <select name='roast' defaultValue='Light' required>
           <option value='Light'>Light Roast</option>
           <option value='Medium'>Medium Roast</option>
           <option value='Dark'>Dark Roast</option>
         </select>
         <input type='number' name='weight' placeholder='burlap sack in lb' defaultValue='130' min='0' max='500'/>
-        <input type='number' name='price' placeholder='Price'required/>
+        <input type='number' name='price' placeholder='Price' defaultValue={defaultValues ? defaultValues.price : ''} required/>
 
         <button type='submit'>{props.buttonText}</button>
       </form>
@@ -23,7 +26,8 @@ function ReusableForm(props) {
 
 ReusableForm.propTypes = {
   formSubmissionHandler: PropTypes.func,
-  buttonText: PropTypes.string
+  buttonText: PropTypes.string,
+  defaultValues: PropTypes.object
 }
 
 export default ReusableForm;
