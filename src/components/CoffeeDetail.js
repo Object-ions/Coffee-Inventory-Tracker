5175418cd12927fa53d0a6183dd25e708eb05907
object-ions <moshikoatia@gmail.com>
2023-11-19 16:56:40 -0800

    Add button to 'sell pound' in 'Details'

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -3,17 +3,22 @@ import PropTypes from 'prop-types';
 
 function CoffeeDetail(props) {
 
-  const { coffee, onClickingDelete } = props;
+  const { coffee, onClickingDelete, onSellPound} = props;
 
   return (
-    <div className="detail">
+    <div>
       <h1>Coffee Detail</h1>
       <h3>Name : <b>{coffee.name}</b></h3>
       <h3>Origin: <b>{coffee.origin}</b></h3>
       <h3>Roast: <b>{coffee.roast}</b></h3>
+      <h3>Weight: <b>{coffee.weight}</b> pounds</h3>
       <h3>Price: <b>{coffee.price}</b> $ per pound</h3>
-      <button onClick={ props.onClickingEdit }>Update Coffee Item</button>
-      <button onClick = { () => onClickingDelete(coffee.id) }>Delete Coffee Item</button>
+      <button onClick = { () => onSellPound(coffee.id) }>Sell 1 pound</button>
+
+      <div className="detail">
+        <button onClick={ props.onClickingEdit }>Update Coffee Item</button>
+        <button onClick = { () => onClickingDelete(coffee.id) }>Delete Coffee Item</button>
+      </div>
     </div>
   );
 }
@@ -21,7 +26,8 @@ function CoffeeDetail(props) {
 CoffeeDetail.propTypes = {
   coffee: PropTypes.object,
   onClickingEdit: PropTypes.func,
-  onClickingDelete: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onSellPound: PropTypes.func
 };
 
 export default CoffeeDetail;
